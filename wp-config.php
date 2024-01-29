e79fc36fa82ff6fc4c3b7ea842a55ab15244da2a
Jacob <jacob.atam@gmail.com>
2024-01-29 14:23:11 -0800

    fix

--- wp-config.php
+++ wp-config.php
@@ -68,12 +68,12 @@ $table_prefix = 'wpwn_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', true);
+//define('WP_DEBUG', true);
 
-define('WP_DEBUG_DISPLAY', true);
+//define('WP_DEBUG_DISPLAY', true);
 
 
-define('WP_DEBUG_LOG', true);
+//define('WP_DEBUG_LOG', true);
 
 
 /* That's all, stop editing! Happy publishing. */
